0200481f152967bad38f0e89d69ba2e50909483e
Hturi <ghaniturismo@gmail.com>
2017-12-15 20:40:52 +0100

    [FIX] warning set

--- descripteur.m
+++ descripteur.m
@@ -3,9 +3,10 @@ function D = descripteur(vDist_Baricentre)
     %nbCoeffsAGarder = 50;
         
     F = fft(vDist_Baricentre);
-    D=F;
+    
     %F = F(1:nbCoeffsAGarder); % On garde les fréquences faibles
     %f0 = abs(F(1)); % normaliser
     
     %D = abs(F) ./ f0;
+    D = abs(F);
 end
